e00336c3445e107a7139f18ce658f0fd34610665
Kiss Shot <72368646+K1ssSh0t@users.noreply.github.com>
2025-04-30 21:41:01 +0000

    fix: implement routing structure in main application file

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { HashRouter } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import { ThemeProvider } from '@/components/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
     <HashRouter>
       <StrictMode>
-        <App />
+        <Routes>
+          <Route path="/" element={<App />} />
+          {/* Add more routes here as needed */}
+          {/* Example: <Route path="/about" element={<About />} /> */}
+
+        </Routes>
       </StrictMode>
     </HashRouter>
   </ThemeProvider>
